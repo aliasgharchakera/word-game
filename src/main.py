88160fec7ac639dbacc7326ecd7c6e2721a6fab7
princebiscuits <ay06993@st.habib.edu.pk>
2022-05-15 14:41:05 +0500

    starting

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
 import csv
 from temp import WAVL
 
-f = open('urbandict-csv.csv', 'rt')
-dic = csv.reader(f)
-wavl = WAVL()
 def tokenize(word):
     new = ""
     for i in word:
         if i not in "[]\'":
             new += i
     return new.lower()
-for word in dic:
-    # word = word.lower()
-    word = tokenize(word)
-    wavl.insert(word)
+
+def loadData(path, wavl):
+    f = open(path, 'rt')
+    dic = csv.reader(f)
+    for word in dic:
+        # word = word.lower()
+        word = tokenize(word)
+        wavl.insert(word)
+print("Loading data...")
+wavl = WAVL()
+loadData('urbandict-csv.csv', wavl)
+print("Loaded data successfully")
 query = input("What word u want to search")
 while (query != "q"):
     print(wavl.search(query))
