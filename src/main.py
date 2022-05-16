f9f30e2fb71b3491e1a3633a916bc2aa10f8a30c
princebiscuits <ay06993@st.habib.edu.pk>
2022-05-16 21:08:10 +0500

    wordcheck

--- src/main.py
+++ src/main.py
@@ -1,45 +1,59 @@
-import csv
-import nltk
-import json
-nltk.download()
+# import csv
+# import nltk
+# import json
+# nltk.download()
 from random import randint, randrange
 from wavl import WAVL
-
 from nltk.corpus import words
+from typing import Any
 
-def tokenize(word):
+def tokenize(word: Any) -> str:
     new = ""
     for i in word:
         if i not in "[]\'":
             new += i
     return new.lower()
 
-def loadData(path, wavl):
+def loadData(path: Any, wavl: WAVL):
     # f = open(path, 'rt')
-    f = open(path)
-    dic = json.load(f)
+    # f = open(path)
+    # dic = json.load(f)
     # dic = csv.reader(f)
-    # dic = words.words()
+    dic = words.words()
+    i, j = 0, 0
     for word in dic:
+        i+= 1
         word = tokenize(word)
         if not wavl.search(word):
+            j += 1
             wavl.insert(word)
+    # print(i, j)
 
-def search(wavl):
-    query = input("What word u want to search")
+def search(wavl: WAVL) -> None:
+    query = input("What word u want to search: ")
     while (query != "q"):
         print(wavl.search(query))
-        if wavl.search(query):
-            wavl.remove(query)
-            print("query removed from tree")
-        query = input("What word u want to search")
+        query = input("What word u want to search: ")
 
 def alphabet_generator():
     alpha = []
-    abc = "abcdefghijklmnopqrstuvwxyz"
+    vowels = "aeiou"
+    abc = "bcdfghjklmnpqrstvwxyz"
+
     for i in range(12):
-        alpha.append(abc[randint(0, 25)])
+        a = randint(0, 1)
+        if a:
+            alpha.append(abc[randint(0, 20)])
+        else:
+            alpha.append(vowels[randint(0, 4)])
     return alpha
+
+def wordcheck(word: str, done: list, wavl: WAVL) -> bool:
+    if wavl.search(word):
+        wavl.remove(word)
+        done.append(word)
+        return True
+    return False
 
 # def words_possible(words, wavl):
 #     for i in range(len(words)):
@@ -52,7 +66,6 @@ wavl = WAVL()
 loadData('urbandict-csv.csv', wavl)
 print("Loaded data successfully")
 search(wavl)
-# wavl.inorder()
-# for i in range(5):
-#     alph = alphabet_generator()
-#     print(alph)
+for i in range(5):
+    alph = alphabet_generator()
+    print(alph)
